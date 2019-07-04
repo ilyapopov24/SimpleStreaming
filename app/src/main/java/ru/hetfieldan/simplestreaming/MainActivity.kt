5bfa2e82cf87e8259dfa8c0c43a03b33621cba7f
hetfieldan24 <ilyapopov24@yandex.ru>
2019-07-04 11:13:52 +0300

    Первый коммит

---
+++ app/src/main/java/ru/hetfieldan/simplestreaming/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.hetfieldan.simplestreaming
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
